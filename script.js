1180b4a7c3c1aaba2c41e16cb307f669a3772b68
Jovin Renny <100133308+jovinrenny@users.noreply.github.com>
2025-08-04 12:22:11 +0530

    updated the basic functionality of the web app
    Updated the functionality of the expense-tracker just to calculate the total income, expense and balance. I want to add more features soon

--- script.js
+++ script.js
@@ -1,16 +1,48 @@
-const incomeForm = document.getElementById("income-form")
-const incomeSection = document.getElementById("income-section")
-const expenseSection = document.getElementById("expense-section")
-
 let totalIncome = 0
+let totalExpense = 0
+let balance = 0
 
-incomeForm.addEventListener("submit", (e)=> {
+let categoryTotals = {
+    Fuel : 0,
+    Food : 0,
+    Cinema : 0,
+    Outing : 0,
+    Other : 0
+}
+
+const transactions = []
+
+
+document.getElementById("income-form").addEventListener("submit", (e)=> {
     e.preventDefault()
-    const incomeInput = document.getElementById("income-amount")
-    totalIncome = parseFloat(incomeInput.value)
-
-    if(totalIncome > 0) {
-        incomeSection.style.display = "none"
-        expenseSection.style.display = "block"
+    const income = parseFloat(document.getElementById("income-amount").value)
+    if(income > 0) {
+        totalIncome = income
+        updateSummary()
+        document.getElementById("income-section").style.display = "none"
+        document.getElementById("expense-section").style.display = "block"
     }
 })
+
+document.getElementById("expense-form").addEventListener("submit", (e) => {
+    e.preventDefault()
+    const expenseAmount = parseFloat(document.getElementById("expense-amount").value)
+    const category = document.getElementById("category").value
+    const description = document.getElementById("expense-description").value || "No description"
+    const date = document.getElementById("expense-date").value
+
+    if(expenseAmount > 0 && category && date) {
+        transactions.push({expenseAmount, category, description, date})
+        categoryTotals[category] += expenseAmount
+        totalExpense += expenseAmount
+        updateSummary()
+        e.target.reset()
+    }
+})
+
+function updateSummary() {
+    const balance = totalIncome - totalExpense
+    document.querySelector(".summary-item.income").textContent = `💰 Income: ₹${totalIncome}`
+    document.querySelector(".summary-item.expense").textContent = `💸 Expense: ₹${totalExpense}`
+    document.querySelector(".summary-item.balance").textContent = `📊 Balance: ₹${balance}`
+}
