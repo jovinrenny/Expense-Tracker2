d95a310d46d07e8f2c5533990877565fa06103cf
Jovin Renny <100133308+jovinrenny@users.noreply.github.com>
2025-08-05 22:56:50 +0530

    Added local storage to save the data user entered

--- script.js
+++ script.js
@@ -12,6 +12,8 @@ let categoryTotals = {
 
 const transactions = []
 
+loadFromLocalStorage()
+
 
 document.getElementById("income-form").addEventListener("submit", (e)=> {
     e.preventDefault()
@@ -19,6 +21,7 @@ document.getElementById("income-form").addEventListener("submit", (e)=> {
     if(income > 0) {
         totalIncome = income
         updateSummary()
+        saveToLocalStorage()
         document.getElementById("income-section").style.display = "none"
         document.getElementById("expense-section").style.display = "block"
     }
@@ -36,13 +39,79 @@ document.getElementById("expense-form").addEventListener("submit", (e) => {
         categoryTotals[category] += expenseAmount
         totalExpense += expenseAmount
         updateSummary()
+        renderTransactions()
+        saveToLocalStorage()
         e.target.reset()
     }
 })
 
 function updateSummary() {
-    const balance = totalIncome - totalExpense
+    balance = totalIncome - totalExpense
     document.querySelector(".summary-item.income").textContent = `💰 Income: ₹${totalIncome}`
     document.querySelector(".summary-item.expense").textContent = `💸 Expense: ₹${totalExpense}`
     document.querySelector(".summary-item.balance").textContent = `📊 Balance: ₹${balance}`
 }
+
+function renderTransactions() {
+    const list = document.getElementById("expenses-list")
+    list.innerHTML = ""
+
+    transactions.forEach((tx, index) => {
+        const item = document.createElement("div")
+        item.className = "expense-item"
+        item.innerHTML = `
+        <strong>${tx.category}</strong>: ₹${tx.expenseAmount}<br>
+        <small>${tx.description} - ${tx.date}</small>
+        <button onclick="showDetails(${index})">Details</button>`
+
+        list.appendChild(item)
+    })
+}
+
+function showDetails(index) {
+    const tx = transactions[index]
+    alert(
+        `Category: ${tx.category}\nAmount: ₹${tx.expenseAmount}\nDescription: ${tx.description}\nDate: ${tx.date}`
+    )
+}
+
+// saved to local storage to fetch later use
+
+function saveToLocalStorage() {
+    localStorage.setItem("totalIncome", totalIncome)
+    localStorage.setItem("transactions", JSON.stringify(transactions))
+    localStorage.setItem("categoryTotals", JSON.stringify(categoryTotals))
+}
+
+// fetching saved data from the local storage
+
+function loadFromLocalStorage() {
+    const savedIncome = localStorage.getItem("totalIncome")
+    const savedTransactions = localStorage.getItem("transactions")
+    const savedCategoryTotal = localStorage.getItem("categoryTotals")
+
+    if(savedIncome) totalIncome = parseFloat(savedIncome)
+    if(savedTransactions) transactions.push(...JSON.parse(savedTransactions))
+    if(savedCategoryTotal) Object.assign(categoryTotals, JSON.parse(savedCategoryTotal))
+
+    totalExpense = transactions.reduce((acc, tx) => acc + tx.expenseAmount, 0)
+
+    updateSummary()
+    renderTransactions()
+
+    if (totalIncome > 0) {
+    document.getElementById("income-section").style.display = "none";
+    document.getElementById("expense-section").style.display = "block";
+    } else {
+    document.getElementById("income-section").style.display = "block";
+    document.getElementById("expense-section").style.display = "none";
+    }
+}
+
+document.getElementById("reset-btn").addEventListener("click", ()=> {
+    const confirmClear = confirm("Are you sure you want to clear all saved data")
+    if(confirmClear) {
+       localStorage.clear()
+        location.reload()
+    }
+})
