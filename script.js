665d1ccbde32e5336c52c8139f385acea3d173b8
Jovin Renny <100133308+jovinrenny@users.noreply.github.com>
2025-08-11 20:57:57 +0530

    hide clear button when there is no dat

--- script.js
+++ script.js
@@ -85,6 +85,7 @@ function saveToLocalStorage() {
     localStorage.setItem("totalIncome", totalIncome)
     localStorage.setItem("transactions", JSON.stringify(transactions))
     localStorage.setItem("categoryTotals", JSON.stringify(categoryTotals))
+    toggleClearBtn()
 }
 
 // fetching saved data from the local storage
@@ -102,6 +103,7 @@ function loadFromLocalStorage() {
 
     updateSummary()
     renderTransactions()
+    toggleClearBtn()
 
     if (totalIncome > 0) {
     document.getElementById("income-section").style.display = "none";
@@ -112,12 +114,26 @@ function loadFromLocalStorage() {
     }
 }
 
+// to show the clear button 
+
+function toggleClearBtn() {
+    const clearBtn = document.getElementById("clear-btn")
+
+    if(localStorage.length > 0) {
+        clearBtn.style.display = "inline-block"
+    } else {
+        clearBtn.style.display = "none"
+    }
+}
+
+
 document.getElementById("clear-btn").addEventListener("click", () => {
     modal.style.display = "flex"
 })
 
 confirmClearBtn.addEventListener("click", ()=> {
     localStorage.clear()
+    toggleClearBtn()
     location.reload()
 })
 
